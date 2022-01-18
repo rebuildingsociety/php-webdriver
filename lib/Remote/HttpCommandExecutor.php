b77a2b191be0316280ae9ba3a442af2aa19d2603
Alex L. Demidov <alexeydemidov@gmail.com>
2022-01-18 13:25:05 +0400

    Increase request timeout to 360000 ms

--- lib/Remote/HttpCommandExecutor.php
+++ lib/Remote/HttpCommandExecutor.php
@@ -208,7 +208,7 @@ class HttpCommandExecutor implements WebDriverCommandExecutor
         curl_setopt($this->curl, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($this->curl, CURLOPT_FOLLOWLOCATION, true);
         curl_setopt($this->curl, CURLOPT_HTTPHEADER, static::DEFAULT_HTTP_HEADERS);
-        $this->setRequestTimeout(30000);
+        $this->setRequestTimeout(360000);
         $this->setConnectionTimeout(30000);
     }
 
